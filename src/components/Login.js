5027643584ef07d8e7fec934992e25706bb8e409
moz <moazzam.khan@realpage.com>
2019-08-24 23:08:21 -0500

    Login ui

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button, TextField} from "@material-ui/core";
 
 
 class Login extends React.Component {
@@ -6,8 +7,24 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                Login component
+            <div style={{ textAlign: 'center' , marginTop: '20%', }}>
+                <div style={{ width: '400px', textAlign: 'left',  marginLeft: 'auto', marginRight: 'auto'}}>
+                    <div>
+                        <label>Username:</label>
+                    </div>
+                    <div>
+                        <TextField Field placeholder="Your username goes here"/>
+                    </div>
+                    <div>
+                        Password:
+                    </div>
+                    <div>
+                        <TextField placeholder="Your password goes here" />
+                    </div>
+                    <div>
+                        <Button color="primary" variant="contained">Login</Button>
+                    </div>
+                </div>
             </div>
         );
     }
