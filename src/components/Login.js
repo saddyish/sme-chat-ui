6f5e91a1646e390fe9c19e6a72c114344a9e307e
moz <moazzam.khan@realpage.com>
2019-08-24 22:49:11 -0500

    Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+
 
     render() {
         return (
             <div>
-
+                Login component
             </div>
         );
     }
